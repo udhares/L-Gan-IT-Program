380df9a9f6b62afdc40a9d186949fcddef9a62ff
udhares <46632861+udhares@users.noreply.github.com>
2023-01-25 23:58:47 +0500

    Create script.js

---
+++ Javascript/script.js
@@ -0,0 +1,23 @@
+window.alert("Bismillaah");
+
+
+console.log("ABCD");
+
+let a;
+a=3;
+let b=5;
+
+let age=20;                  //numbers
+let firstname = "Aishath"   //string
+let std = true;                 //boolean
+
+
+console.log(a);
+console.log(b);
+
+console.log("Assalaamualaikum", firstname);
+console.log("Your age is:", age);
+console.log("Your student status is:", std);
+
+document.getElementById("xyz").style.color="red";
+
